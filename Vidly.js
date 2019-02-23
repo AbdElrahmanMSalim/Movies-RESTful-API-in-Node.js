e860baa94d86c98fe47508bbba06fde3d23030ca
abdelrahmanmsalim <abdelrahman.m.salim@gmail.com>
2019-02-23 18:42:18 +0200

    fixed untracked files

--- Vidly.js
+++ Vidly.js
@@ -10,7 +10,7 @@ require('./startup/validation')();
 require('./startup/prod')(app);
 
 
-const port = process.env.port;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
 module.exports = server;
