1fe325a31983afe78ed334adad9844de224f418d
abdelrahmanmsalim <abdelrahman.m.salim@gmail.com>
2019-09-13 17:37:21 +0200

    Fixing some typo

--- models/movies.js
+++ models/movies.js
@@ -1,41 +1,56 @@
-const mongoose = require('mongoose');
-const {genresSchema} = require('../models/genres');
-const joi = require('joi');
+const mongoose = require("mongoose");
+const { genresSchema } = require("../models/genres");
+const joi = require("joi");
 
-const Movies = mongoose.model('movies', new mongoose.Schema({
+const Movies = mongoose.model(
+  "movies",
+  new mongoose.Schema({
     title: {
-        type: String,
-        trim: true,
-        minlength: 5,
-        maxlength: 20,
-        required: true
+      type: String,
+      trim: true,
+      minlength: 5,
+      maxlength: 20,
+      required: true
     },
     genre: {
-        type: genresSchema,
-        required: true
+      type: genresSchema,
+      required: true
     },
     numberInStock: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 255
+      type: Number,
+      required: true,
+      min: 0,
+      max: 255
     },
     dailyRentalRate: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 255
+      type: Number,
+      required: true,
+      min: 0,
+      max: 255
     }
-}));
+  })
+);
 
-function validateMovie(movie){
-    const schema = {
-        title: joi.string().min(5).max(20).required(),
-        genreID: joi.objectId().required(),
-        numberInStock: joi.number().min(0).max(255).required(),
-        dailyRentalRate: joi.number().min(0).max(255).required()
-    };
-    return joi.validate(movie, schema);
+function validateMovie(movie) {
+  const schema = {
+    title: joi
+      .string()
+      .min(5)
+      .max(20)
+      .required(),
+    genreId: joi.objectId().required(),
+    numberInStock: joi
+      .number()
+      .min(0)
+      .max(255)
+      .required(),
+    dailyRentalRate: joi
+      .number()
+      .min(0)
+      .max(255)
+      .required()
+  };
+  return joi.validate(movie, schema);
 }
 
 exports.Movies = Movies;
--- routes/movies.js
+++ routes/movies.js
@@ -1,81 +1,78 @@
-const auth = require('../middleware/auth');
-const express = require('express');
+const auth = require("../middleware/auth");
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const {Movies, validateMovies} = require('../models/movies')
-const {Genres} = require('../models/genres')
+const mongoose = require("mongoose");
+const { Movies, validateMovie } = require("../models/movies");
+const { Genres } = require("../models/genres");
 
+router.get("/", async (req, res) => {
+  const movies = await Movies.find().sort("name");
 
-router.get('/', async (req, res) => {
-    const movies = await Movies.find().sort('name');
-
-    res.send(movies);
+  res.send(movies);
 });
 
-router.get('/:id', async (req, res)=>{
-    const movie = await Movies.findById(req.params.id);
+router.get("/:id", async (req, res) => {
+  const movie = await Movies.findById(req.params.id);
 
-    if(!movie) res.status(404).send("Not Found");
+  if (!movie) res.status(404).send("Not Found");
 
-    res.send(movie);
+  res.send(movie);
 });
 
-router.post('/', auth, async (req, res)=> {
-    const {error} = validateMovies(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+router.post("/", auth, async (req, res) => {
+  const { error } = validateMovie(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    const genre = await Genres.findById(req.body.genreID);
-    if(!genre) return res.status(400).send('Invalid genre.');
-    
-    const movie = new Movies({
-        title: req.body.title,
-        genre: {
-            _id: genre._id,
-            name: genre.name
-        },
-        numberInStock: req.body.numberInStock,
-        dailyRentalRate: req.body.dailyRentalRate
-    });
+  const genre = await Genres.findById(req.body.genreId);
+  if (!genre) return res.status(400).send("Invalid genre.");
 
-    await movie.save();
+  const movie = new Movies({
+    title: req.body.title,
+    genre: {
+      _id: genre._id,
+      name: genre.name
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate
+  });
 
-    res.send(movie);
+  await movie.save();
+
+  res.send(movie);
 });
 
-router.put('/:id', auth, async (req, res)=>{
-    const {error} = validateMovies(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    
-    const genre = await Genres.findById(req.body.genreID);
-    if(!genre) return res.status(400).send('Invalid genre.');
-    
-    const movie = await Movies.findByIdAndUpdate(req.params.id, 
-        { 
-        title: req.body.title,
-        genre: 
-        {
-            _id: genre._id,
-            name: genre.name
-        },
-        numberInStock: req.body.numberInStock,
-        dailyRentalRate: req.body.dailyRentalRate },
-        { new: true})
-    
-    if(!movie) return res.status(404).send("given id not found");
-    
-    res.send(movie);     
+router.put("/:id", auth, async (req, res) => {
+  const { error } = validateMovie(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
+  const genre = await Genres.findById(req.body.genreId);
+  if (!genre) return res.status(400).send("Invalid genre.");
+
+  const movie = await Movies.findByIdAndUpdate(
+    req.params.id,
+    {
+      title: req.body.title,
+      genre: {
+        _id: genre._id,
+        name: genre.name
+      },
+      numberInStock: req.body.numberInStock,
+      dailyRentalRate: req.body.dailyRentalRate
+    },
+    { new: true }
+  );
+
+  if (!movie) return res.status(404).send("given id not found");
+
+  res.send(movie);
 });
 
-router.delete('/:id', auth, async (req, res)=>{
+router.delete("/:id", auth, async (req, res) => {
+  const movie = await Movies.findByIdAndRemove(req.params.id);
 
-    const movie = await Movies.findByIdAndRemove(req.params.id)
-    
-    if(!movie) return res.status(404).send("given id not found");
-    
-    res.send(movie);
-     
+  if (!movie) return res.status(404).send("given id not found");
 
+  res.send(movie);
 });
 
 module.exports = router;
